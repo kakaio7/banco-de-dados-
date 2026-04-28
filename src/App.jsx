cc42f6c5d9873181c220836aae572536c7510d20
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2026-04-28 16:53:25 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react';
+import { supabase } from './supabase';
+
+export default function App() {
+
+  //Estados de autenticação
+  const [ user, setUser ] = useState(null);
+  const [ email, setEmail ] = useState('');
+  const [ senha, setSenha ] = useState('');
+  
+  //Estados da tarefa
+  const [ tarefas, setTarefas ] = useState([]);
+  const [ titulo, setTitulo ] = useState('');
+  const [ descricao, setDescricao ] = useState('');
+
+  useEffect(function(){
+    
+    async function carregarSessao(){
+      const resposta = await supabase.auth.getSession();
+      const sessao = resposta.data.session;
+
+      if(sessao){
+        setUser(sessao.user);
+      } else {
+        setUser(null);
+      }
+    }
+    carregarSessao();
+
+    const listener = supabase.auth.onAuthStateChange(
+      function(evento, sessao) {
+        if(sessao){
+          setUser(sessao.user);
+        } else {
+          setUser(null);
+        }
+      }
+    );
+
+    return function(){
+      listener.data.subscription.unsubscribe();
+    }
+  }, []);
+
+  useEffect(function(){
+    if(user){
+      buscarTarefas();
+    } else {
+      setTarefas([]);
+    }
+  }, [user]);
+
+  async function cadastrar() {
+    const resposta = await supabase.auth.signUp(
+      {
+        email: email,
+        password: senha
+      }
+    );
+
+    if(resposta.error){
+      alert('Erro ao cadastrar: ' + resposta.error.message);
+    } else {
+      alert('Mais um pro time! Se vira, menó');
+    }
+  }
+
+  async function login() {
+    const resposta = await supabase.auth.signInWithPassword(
+      {
+        email: email,
+        password: senha,
+      }
+    );
+  }
+
+  async function logout(){
+    await supabase.auth.signOut();
+  }
+
+  if(!user){
+    return (
+      <div>
+        <h1>da nada</h1>
+        <h2>Login/Cadastro</h2>
+        <div>
+          <input type="email" placeholder=  'coloque aqui o seu email por obséquio' 
+          value={email} onChange={e => setEmail(e.target.value)}
+          />
+          <input type="senha" placeholder= 'coloque aqui a sua senha por obséquio' 
+          value={senha} onChange={e => setSenha(e.target.value)}
+          />
+          <button onClick={login}>entrar</button>
+          <button onClick={cadastrar}>cadastrar</button>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div><h1>deu tudo</h1></div>
+  )
+}
